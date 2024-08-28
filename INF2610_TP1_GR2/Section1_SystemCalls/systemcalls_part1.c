284f67dad977dfb8f485ab2b04ef1e4ae92f9e9a
Shanmukh Iyer <shanmukh.iyer@gmail.com>
2024-08-28 14:47:51 -0400

    finished section1

--- INF2610_TP1_GR2/Section1_SystemCalls/systemcalls_part1.c
+++ INF2610_TP1_GR2/Section1_SystemCalls/systemcalls_part1.c
@@ -3,11 +3,29 @@
  * Automne 2024
  * SystemCalls - part1.c
  *
- * Ajoutez vos noms, prénoms et matricules
+ * Shanmukh Iyer 2251372; Michael Imbert 2212593
 */
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main () {
-    // TODO
-    return 0;
+	const char *pathname = "./systemcalls_output2.txt";
+	int flag = O_TRUNC | O_RDWR;
+	int fd = open(pathname, flag);
+	if(fd < 0) {
+		perror("Appel système open a échoué\n");
+		exit(2);
+	}
+	printf("%d\n", fd);
+	write(1, "Saisissez votre texte suivi de CTRL-D : ", 41);
+	
+	char c;
+	while(read(0, &c, 1) > 0) {
+		write(fd, &c, 1);
+	}
+	close(fd);
+	return 0;
 }
--- INF2610_TP1_GR2/Section1_SystemCalls/systemcalls_part2.c
+++ INF2610_TP1_GR2/Section1_SystemCalls/systemcalls_part2.c
@@ -3,23 +3,30 @@
  * Automne 2024
  * SystemCalls - part2.c
  *
- * Ajoutez vos noms, prénoms et matricules
+ * Shanmukh Iyer 2251372; Michael Imbert 2212593
 */
-
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 
 void part21 ()
 {
-    // TODO
+char message[] = "77dbcb01f571f1c32g196c3a7d27f62e (printed using printf)";
+size_t i = 0;
+while(message[i] != '\0') 
+{
+printf("%c", message[i]);
+++i;
+}
 
+write(1, "77dbcb01f571f1c32g196c3a7d27f62e (printed using write)\n", 56);
+printf("\n");
 }
 
 void part22 ()
 {
-    // TODO
-    
+setvbuf(stdout, NULL, _IONBF, 0);
+    part21();
 }
 
 int main (int argc, char* argv[])

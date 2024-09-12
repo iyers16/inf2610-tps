472cf48548f58d868078e9f37af3f081f0c171d1
Shanmukh Iyer <shanmukh.iyer@gmail.com>
2024-09-12 00:12:30 -0400

    abstraction in place for 3.1

--- INF2610_TP1_GR2/Section1_SystemCalls/systemcalls_part2.c
+++ INF2610_TP1_GR2/Section1_SystemCalls/systemcalls_part2.c
@@ -4,45 +4,50 @@
  * SystemCalls - part2.c
  *
  * Shanmukh Iyer 2251372; Michael Imbert 2212593
-*/
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-void part21 ()
+void part21()
 {
-char message[] = "77dbcb01f571f1c32g196c3a7d27f62e (printed using printf)";
-size_t i = 0;
-while(message[i] != '\0') 
-{
-printf("%c", message[i]);
-++i;
+    char message[] = "77dbcb01f571f1c32g196c3a7d27f62e (printed using printf)";
+    size_t i = 0;
+    while (message[i] != '\0')
+    {
+        printf("%c", message[i]);
+        ++i;
+    }
+
+    write(1, "77dbcb01f571f1c32g196c3a7d27f62e (printed using write)\n", 56);
+    printf("\n");
 }
 
-write(1, "77dbcb01f571f1c32g196c3a7d27f62e (printed using write)\n", 56);
-printf("\n");
-}
-
-void part22 ()
+void part22()
 {
-setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
     part21();
 }
 
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    
-    if (argc!=2)
-    {   printf("Le programme a un seul paramètre : 1 ou 2\n");
+
+    if (argc != 2)
+    {
+        printf("Le programme a un seul paramètre : 1 ou 2\n");
         return 1;
     }
-    switch (atoi(argv[1])) {
-         case 1:        part21();
-                        break;
-         case 2:        part22();
-                        break;
-        default:        printf(" Le paramètre du programme est invalide\n");
-                        return 1;
+    switch (atoi(argv[1]))
+    {
+    case 1:
+        part21();
+        break;
+    case 2:
+        part22();
+        break;
+    default:
+        printf(" Le paramètre du programme est invalide\n");
+        return 1;
     }
     return 0;
 }
--- INF2610_TP1_GR2/Section2_Processes/processes_part1.c
+++ INF2610_TP1_GR2/Section2_Processes/processes_part1.c
@@ -8,10 +8,32 @@
 
 #include "libprocesslab/libprocesslab.h"
 
+void spawn_child_processes(int n)
+{
+    pid_t child_pid;
+
+    //Father code (before child processes start)
+
+    for (int i = 0; i < n; i++) {
+        if ((child_pid = fork()) == 0) {
+            //child code
+            printf("\nPID=%d\t PPID=%d\n", getpid(), getppid());
+            exit(0);
+        }
+    }
+
+    while (wait(NULL) > 0); // this way, the father waits for all the child processes 
+
+    //Father code (After all child processes end)
+    // printf("\nPID=%d\t PPID=%d\n", getpid(), getppid());
+}
 
 void question1()
 {
-    // TODO
-    
+    for (int i = 0; i < 2 ; ++i)
+    {
+        spawn_child_processes(2);
+    }
+    printf("\nPID=%d\t PPID=%d\n", getpid(), getppid());
 }
 

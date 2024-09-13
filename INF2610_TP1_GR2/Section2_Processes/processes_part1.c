eb7adc5e50b97dc09701170491942f906d29c381
Shanmukh Iyer <shanmukh.iyer@gmail.com>
2024-09-13 01:01:22 -0400

    got printproc working

--- INF2610_TP1_GR2/Section2_Processes/processes_part1.c
+++ INF2610_TP1_GR2/Section2_Processes/processes_part1.c
@@ -8,59 +8,74 @@
 
 #include "libprocesslab/libprocesslab.h"
 
-void spawn_child_processes(int level, int num_children, int ctr)
-{
-    pid_t child_pid;
-
-    for (int i = 1; i <= num_children; i++)
-    {
-        if ((child_pid = fork()) == 0)
-        {
-            registerProc(getpid(), getppid(), 2, ctr+i-1);
-            printf("\n\t\tLevel %d.%d\t PID=%d\t PPID=%d\n", level, ctr + i - 1, getpid(), getppid());
-            _exit(0);
-        }
-        waitpid(child_pid, NULL, 0);
-    }
-}
-
 void question1()
 {
-    registerProc(getpid(), getppid(), 0,0);
-    printf("\nLevel 0 PID=%d\n", getpid());
-    int ctr = 1;
-    pid_t kids1 = fork();
-    if (kids1 == 0)
+    registerProc(getpid(), getppid(), 0, 0);
+    pid_t proc11, proc12, proc13;
+
+    if ((proc11 = fork()) == 0)
     {
         registerProc(getpid(), getppid(), 1, 1);
-        printf("\n\tLevel %d.%d\t PID=%d\t PPID=%d\n", 1, 1, getpid(), getppid());
-        spawn_child_processes(2, 1, ctr);
+        pid_t proc21;
+        if((proc21 = fork()) == 0)
+        {
+            registerProc(getpid(), getppid(), 2, 1);
+            _exit(0);
+        }
+        waitpid(proc21, NULL, 0);
         _exit(0);
     }
-    ctr += 1;
-    waitpid(kids1, NULL, 0);
+    waitpid(proc11, NULL, 0);
 
-    pid_t kids2 = fork();
-    if (kids2 == 0)
+    if ((proc12 = fork()) == 0)
     {
         registerProc(getpid(), getppid(), 1, 2);
-        printf("\n\tLevel %d.%d\t PID=%d\t PPID=%d\n", 1, 2, getpid(), getppid());
-        spawn_child_processes(2, 1, ctr);
+        pid_t proc22;
+        if((proc22 = fork()) == 0)
+        {
+            registerProc(getpid(), getppid(), 2, 2);
+            _exit(0);
+        }
+        waitpid(proc22, NULL, 0);
         _exit(0);
     }
-    ctr += 1;
-    waitpid(kids2, NULL, 0);
+    waitpid(proc12, NULL, 0);
 
-    pid_t kids3 = fork();
-    if (kids3 == 0)
+    if ((proc13 = fork()) == 0)
     {
         registerProc(getpid(), getppid(), 1, 3);
-        printf("\n\tLevel %d.%d\t PID=%d\t PPID=%d\n", 1, 3, getpid(), getppid());
-        spawn_child_processes(2, 4, ctr);
+        pid_t proc23, proc24, proc25, proc26;
+        if((proc23 = fork()) == 0)
+        {
+            registerProc(getpid(), getppid(), 2, 3);
+            _exit(0);
+        }
+        waitpid(proc23, NULL, 0);
+
+        if((proc24 = fork()) == 0)
+        {
+            registerProc(getpid(), getppid(), 2, 4);
+            _exit(0);
+        }
+        waitpid(proc24, NULL, 0);
+
+        if((proc25 = fork()) == 0)
+        {
+            registerProc(getpid(), getppid(), 2, 5);
+            _exit(0);
+        }
+        waitpid(proc25, NULL, 0);
+
+        if((proc26 = fork()) == 0)
+        {
+            registerProc(getpid(), getppid(), 2, 6);
+            _exit(0);
+        }
+        waitpid(proc26, NULL, 0);
         _exit(0);
     }
-    ctr += 3;
-    waitpid(kids3, NULL, 0);
+    waitpid(proc13, NULL, 0);
+
     printProcRegistrations();
-    _exit(0);
+    return;
 }

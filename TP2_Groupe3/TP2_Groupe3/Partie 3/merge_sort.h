363b8d448b5c80135a915257df79558b72e850ef
Michael Imbert <miimb@l4712-08.info.polymtl.ca>
2024-10-29 19:42:19 -0400

    question 1 part 3 done

---
+++ TP2_Groupe3/TP2_Groupe3/Partie 3/merge_sort.h
@@ -0,0 +1,40 @@
+#include <sys/mman.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h> 
+
+
+
+#define MAX_ARRAY_SIZE 1000000
+#define MAX_NUM_SIZE 1000000
+#define SEM_NAME "/merge_sort_sem"
+
+#ifndef MAP_ANONYMOUS
+#define MAP_ANONYMOUS 0x20
+#endif
+
+typedef struct {
+    int* array;
+    int size;
+} SharedData;
+
+
+SharedData *shared_data;
+sem_t* mutex;
+int fd;
+struct timeval start_time, end_time;
+
+void merge_sort( int left, int right);
+void pp_final_merge(int start, int mid, int num_processes, int part_size);
+void merge(int left, int mid, int right);
+void execute_merge_sort(int start, int end, int num_processes);
+void show_array();
+void write_array_into_file();
---
+++ TP2_Groupe3/TP2_Groupe3/Partie 3/merge_sort_upgrade.c
@@ -0,0 +1,113 @@
+#include "merge_sort.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <array_size> <num_processes>\n", argv[0]);
+        exit(1);
+    }
+
+    int array_size = atoi(argv[1]);
+    int num_processes = atoi(argv[2]);
+
+    shared_data = mmap(NULL, sizeof(SharedData) + sizeof(int) * array_size, PROT_READ | PROT_WRITE, MAP_SHARED, -1, 0);
+
+    shared_data->size = array_size;
+    shared_data->array = (int*)(shared_data + 1);
+    
+    /* Populate the array to test the sort */
+    srand(time(NULL));
+    for (int i = 0; i < array_size; i++) {
+        shared_data->array[i] = rand() % MAX_NUM_SIZE;
+    }
+
+    execute_merge_sort(0, array_size - 1, num_processes);
+
+    show_array();
+    munmap(shared_data, sizeof(SharedData) + array_size * sizeof(int));
+
+    return 0;
+}
+
+void merge_sort( int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        merge_sort(left, mid);
+        merge_sort(mid + 1, right);
+        merge(left, mid, right);
+    }
+}
+
+void merge(int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = shared_data->array[left + i];
+    for (j = 0; j < n2; j++)
+        R[j] = shared_data->array[mid + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            shared_data->array[k] = L[i];
+            i++;
+        } else {
+            shared_data->array[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        shared_data->array[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        shared_data->array[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void execute_merge_sort(int start, int end, int num_processes){
+    int part_size = (end - start + 1) / num_processes; // taille de la partie du array dont le processus se charge
+
+    // on separe en plusieurs partie pour chaque processus qui aura sa propre partie du array a merge_sort
+    for (int i = 0; i < num_processes; i++){
+        if(fork() == 0){
+            int part_start = start + i * part_size;
+            int part_end = ( i == num_processes - 1) ? end : (part_start + part_size - 1);
+            merge_sort(part_start, part_end);
+        }
+        while(wait(NULL) > 0);
+
+        // pp doit fusionner les parties tries par les processus enfants
+        pp_final_merge(start, end, num_processes, part_size);
+    }
+
+}
+
+void pp_final_merge(int start, int end, int num_processes, int part_size) {
+    int mid;
+    for(int i = 1; i < num_processes; i ++) {
+        mid = start + i * part_size - 1;
+        int right = (i == num_processes - 1) ? end : (start + (i + 1) * part_size - 1);
+        merge(start, mid, right);
+    }
+}
+
+void show_array(){
+    printf("Sorted array: ");
+    for (int i = 0; i < shared_data->size; i++) {
+        printf("%d ", shared_data->array[i]);
+    }
+    printf("\n");
+}

d706682d17cd0460ad7b7823ae45ff09007842e9
Michael Imbert <miimb@l4712-08.info.polymtl.ca>
2024-10-29 21:36:11 -0400

    journalisation done

--- TP2_Groupe3/TP2_Groupe3/Partie 3/merge_sort.c
+++ TP2_Groupe3/TP2_Groupe3/Partie 3/merge_sort.c
@@ -8,6 +8,13 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    FILE *file = fopen("sorted_array.txt", "w");
+    if (file == NULL) {
+        perror("fopen");
+        exit(1);
+    }
+    fclose(file);
+
     int array_size = atoi(argv[1]);
     int num_processes = atoi(argv[2]);
 
@@ -16,6 +23,9 @@ int main(int argc, char *argv[]) {
     perror("mmap");
     exit(1);
    }
+
+    mutex = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    sem_init(mutex, 1, 1);
 
     shared_data->size = array_size;
     shared_data->array = (int*)(shared_data + 1);
@@ -28,7 +38,11 @@ int main(int argc, char *argv[]) {
 
     execute_merge_sort(0, array_size - 1, num_processes);
     show_array();
+    printf("Temps d'exécution total: %.6f secondes\n", total_time);
+
+    sem_destroy(mutex);
     munmap(shared_data, sizeof(SharedData) + array_size * sizeof(int));
+    munmap(mutex, sizeof(sem_t));
 
     return 0;
 }
@@ -39,6 +53,7 @@ void merge_sort( int left, int right) {
         merge_sort(left, mid);
         merge_sort(mid + 1, right);
         merge(left, mid, right);
+        write_array_into_file(left, right, shared_data->array);
     }
 }
 
@@ -85,6 +100,8 @@ void merge(int left, int mid, int right) {
 void execute_merge_sort(int start, int end, int num_processes){
     int part_size = (end - start + 1) / num_processes; // taille de la partie du array dont le processus se charge
 
+    gettimeofday(&start_time, NULL);
+
     // on separe en plusieurs partie pour chaque processus qui aura sa propre partie du array a merge_sort
     for (int i = 0; i < num_processes; i++){
         if(fork() == 0){
@@ -93,12 +110,19 @@ void execute_merge_sort(int start, int end, int num_processes){
             merge_sort(part_start, part_end);
             exit(0);
         }
-        while(wait(NULL) > 0);
-
-        // pp doit fusionner les parties tries par les processus enfants
-        pp_final_merge(start, end, num_processes, part_size);
     }
+    while(wait(NULL) > 0);
 
+    // pp doit fusionner les parties triees par les processus enfants
+    pp_final_merge(start, end, num_processes, part_size);
+
+    gettimeofday(&end_time, NULL);
+    write_array_into_file(start, end, shared_data->array);
+
+    long seconds = end_time.tv_sec - start_time.tv_sec;
+    long microseconds = end_time.tv_usec - start_time.tv_usec;
+    total_time = seconds + microseconds * 1e-6;
+    
 }
 
 void pp_final_merge(int start, int end, int num_processes, int part_size) {
@@ -117,3 +141,27 @@ void show_array(){
     }
     printf("\n");
 }
+
+void write_array_into_file(int start, int end, int *array) {
+    sem_wait(mutex);
+
+    FILE *file = fopen("sorted_array.txt", "a");
+    if (file == NULL) {
+        perror("fopen");
+        exit(1);
+        sem_post(mutex);
+    }
+
+    fprintf(file, "Start = %d, End = %d, sorted = [", start, end);
+    for (int i = start; i <= end; i++) {
+        if (i != start) {
+            fprintf(file, ", ");
+        }
+        fprintf(file, "%d", array[i]);
+    }
+    fprintf(file, "]\n");
+
+    fclose(file);
+    sem_post(mutex);
+}
+
--- TP2_Groupe3/TP2_Groupe3/Partie 3/merge_sort.h
+++ TP2_Groupe3/TP2_Groupe3/Partie 3/merge_sort.h
@@ -14,7 +14,7 @@
 
 
 #define MAX_ARRAY_SIZE 1000000
-#define MAX_NUM_SIZE 1000000
+#define MAX_NUM_SIZE 100000
 #define SEM_NAME "/merge_sort_sem"
 
 #ifndef MAP_ANONYMOUS
@@ -31,10 +31,11 @@ SharedData *shared_data;
 sem_t* mutex;
 int fd;
 struct timeval start_time, end_time;
+double total_time;
 
 void merge_sort( int left, int right);
 void pp_final_merge(int start, int mid, int num_processes, int part_size);
 void merge(int left, int mid, int right);
 void execute_merge_sort(int start, int end, int num_processes);
 void show_array();
-void write_array_into_file();
+void write_array_into_file(int start, int end, int *array);

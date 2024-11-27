6132bcb011f72e433b35ef6372026675f1e05367
iyers16 <shanmukh.iyer@gmail.com>
2024-11-26 19:25:25 -0500

    sect 2 done

--- TP3_Groupe3/Section 2/GestionMemoire.c
+++ TP3_Groupe3/Section 2/GestionMemoire.c
@@ -1,47 +1,118 @@
-#include<stdbool.h>
+#include <stdbool.h>
 #include "./libs/lib.h"
 
-unsigned int calculerNumeroDePage(unsigned long adresse) {
-	// TODO
-    return 0;
+#define TAILLE_PAGE 1024
+
+unsigned int calculerNumeroDePage(unsigned long adresse)
+{
+	return adresse / TAILLE_PAGE;
 }
 
-unsigned long calculerDeplacementDansLaPage(unsigned long adresse) {
-	// TODO
-     return 0;
+unsigned long calculerDeplacementDansLaPage(unsigned long adresse)
+{
+	return adresse % TAILLE_PAGE;
 }
 
-unsigned long calculerAdresseComplete(unsigned int numeroDePage, unsigned long deplacementDansLaPage) {
-	// TODO
-     return 0;
+unsigned long calculerAdresseComplete(unsigned int numeroDePage, unsigned long deplacementDansLaPage)
+{
+	return numeroDePage * TAILLE_PAGE + deplacementDansLaPage;
 }
 
-void rechercherTLB(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
-	// TODO
+void rechercherTLB(struct RequeteMemoire *req, struct SystemeMemoire *mem)
+{
+	for (int i = 0; i < TAILLE_TLB; i++)
+	{
+		if (mem->tlb->numeroPage[i] == calculerNumeroDePage(req->adresseVirtuelle) && mem->tlb->entreeValide[i])
+		{
+
+			req->adressePhysique = calculerAdresseComplete(mem->tlb->numeroCadre[i], calculerDeplacementDansLaPage(req->adresseVirtuelle));
+			mem->tlb->dernierAcces[i] = req->date;
+			req->estDansTLB = 1;
+
+			return;
+		}
+	}
+	req->adressePhysique = 0;
+	req->estDansTLB = 0;
 }
 
-void rechercherTableDesPages(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
-	// TODO
+void rechercherTableDesPages(struct RequeteMemoire *req, struct SystemeMemoire *mem)
+{
+	int numPageReq = calculerNumeroDePage(req->adresseVirtuelle);
+	if (mem->tp->entreeValide[numPageReq])
+	{
+
+		req->adressePhysique = calculerAdresseComplete(mem->tp->numeroCadre[numPageReq], calculerDeplacementDansLaPage(req->adresseVirtuelle));
+		req->estDansTablePages = 1;
+
+		return;
+	}
+	req->adressePhysique = 0;
+	req->estDansTablePages = 0;
 }
 
-void ajouterDansMemoire(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
-	//TODO
+void ajouterDansMemoire(struct RequeteMemoire *req, struct SystemeMemoire *mem)
+{
+	int numPageReq = calculerNumeroDePage(req->adresseVirtuelle);
+
+	for (int i = 0; i < TAILLE_MEMOIRE; i++)
+	{
+		if (mem->memoire->utilisee[i] == 0)
+		{
+
+			mem->memoire->numeroPage[i] = numPageReq;
+			mem->memoire->dateCreation[i] = req->date;
+			mem->memoire->dernierAcces[i] = req->date;
+			mem->memoire->utilisee[i] = 1;
+
+			// il faut update la table des pages aussi
+			mem->tp->numeroCadre[numPageReq] = i; // i est num du cadre libre
+			mem->tp->entreeValide[numPageReq] = 1;
+
+			req->adressePhysique = calculerAdresseComplete(i, calculerDeplacementDansLaPage(req->adresseVirtuelle));
+			req->estDansTablePages = 1;
+
+			return;
+		}
+	}
 }
 
-void mettreAJourTLB(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
-	// TODO
+void mettreAJourTLB(struct RequeteMemoire *req, struct SystemeMemoire *mem)
+{
+	int indexToReplace = 0;
+
+	for (int i = 0; i < TAILLE_TLB; i++)
+	{ // une entree invalide est prio, ne marche pas sans cette verif
+		if (mem->tlb->entreeValide[i] == 0)
+		{
+			indexToReplace = i;
+			break;
+		}
+
+		if (mem->tlb->dateCreation[i] < mem->tlb->dateCreation[indexToReplace])
+		{
+			indexToReplace = i;
+		}
+	}
+	mem->tlb->numeroPage[indexToReplace] = calculerNumeroDePage(req->adresseVirtuelle);
+	mem->tlb->numeroCadre[indexToReplace] = calculerNumeroDePage(req->adressePhysique);
+	mem->tlb->entreeValide[indexToReplace] = 1;
+	mem->tlb->dernierAcces[indexToReplace] = req->date;
+	mem->tlb->dateCreation[indexToReplace] = req->date;
+
+	return;
 }
 
 // NE PAS MODIFIER
-int main() {
-    evaluate(
-		&calculerNumeroDePage, 
-		&calculerDeplacementDansLaPage, 
-		&calculerAdresseComplete, 
-        &rechercherTLB, 
+int main()
+{
+	evaluate(
+		&calculerNumeroDePage,
+		&calculerDeplacementDansLaPage,
+		&calculerAdresseComplete,
+		&rechercherTLB,
 		&rechercherTableDesPages,
 		&mettreAJourTLB,
-		&ajouterDansMemoire
-    );
-    return 0;
+		&ajouterDansMemoire);
+	return 0;
 }
